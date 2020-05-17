4a906168e75579a177361529ef3befd27871b382
YSZ0927 <chaufany@gmail.com>
2020-05-17 10:43:42 +0800

    解决less-loader版本兼容问题

--- src/pages/Wechat/index.tsx
+++ src/pages/Wechat/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'antd-mobile';
+import './index.less'
 
 export interface Props {
     name: string;
@@ -11,6 +12,7 @@ class Wechat extends React.Component<Props, object>{
     render() {
         return (
             <div className="hello">
+                <p>asdfasdf</p>
                 <Button type="primary">sdfasasfd</Button>
             </div>
           );
