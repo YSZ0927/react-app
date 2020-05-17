7c1e1533e86e7a2fbb1d6fa9901d3bbc0442a728
yushuizeng <chaufany@gmail.comom>
2020-05-17 14:27:52 +0800

    首页新增轮播图

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Carousel, WingBlank } from 'antd-mobile';
 import SearchInput from '@/components/SearchInput'
+import './index.less'
 
 export interface Props {
     name: string;
@@ -13,20 +14,32 @@ const Home = function () {
         'https://image.sudian178.com/sd/themeImg/23216808188207786.jpg',
         'https://image.sudian178.com/sd/themeImg/23561250090602203.jpg'
     ]
+    const colorArr = [
+        'rgb(255, 173, 0)',
+        'rgb(193, 183, 160)',
+        'rgb(255, 80, 108)',
+        'rgb(255, 172, 77)'
+    ]
+    let color = colorArr[0]
+    const changeBanner = (val) => {
+        color = colorArr[val]
+    }
     return (
         <div className="home">
-            <SearchInput></SearchInput>
-            <WingBlank>
+            <div className="home-banner" style={{background: color}}>
+                <SearchInput></SearchInput>
                 <Carousel
                     autoplay={true}
+                    afterChange={changeBanner}
                     infinite>
                     {imgArr.map(val => (
                         <img src={val}
+                            key={val}
                             alt=""
-                            style={{ width: '100%', verticalAlign: 'top' }}/>
+                            style={{ width: '100%', verticalAlign: 'top', borderRadius: '6px' }}/>
                     ))}
                 </Carousel>
-            </WingBlank>
+            </div>
         </div>
     );
 }
