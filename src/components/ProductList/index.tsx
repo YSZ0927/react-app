495796fa6fb20eb2df8a5ae1fccb1af52b6f8aca
YSZ0927 <chaufany@gmail.com>
2020-06-20 12:06:09 +0800

    新增图片

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,28 +1,28 @@
-import React, { PropsWithChildren, useEffect, useState } from 'react'
+import React, { PropsWithChildren } from 'react' // useEffect, useState
 import { WingBlank } from 'antd-mobile';
 import './index.less'
-import { GoodsList, Goods } from '@/typings'
+import { GoodsList } from '@/typings' // Goods
 
 type Props = PropsWithChildren<{
     goodsList: GoodsList
     getGoodsList: () => void
 }>
 export default function (props: Props) {
-    const [proListleft, getLeftList] = useState<Goods[]>([])
-    const [proListRight, getRightList] = useState<Goods[]>([])
+    // const [proListleft, getLeftList] = useState<Goods[]>([])
+    // const [proListRight, getRightList] = useState<Goods[]>([])
 
-    let listArr:Array<Goods> = []
-    useEffect(() => {
-        listArr = props.goodsList.list
-        if (listArr.length === 0) {
-            props.getGoodsList()
-        }
-        // getRightList(proListRight.concat(listArr.slice(Math.ceil(listArr.length / 2), listArr.length)))
-    })
+    // let listArr:Array<Goods> = []
+    // useEffect(() => {
+    //     listArr = props.goodsList.list
+    //     if (listArr.length === 0) {
+    //         props.getGoodsList()
+    //     }
+    //     getRightList(proListRight.concat(listArr.slice(Math.ceil(listArr.length / 2), listArr.length)))
+    // })
     // getLeftList(proListleft.concat(listArr.slice(0, Math.floor(listArr.length / 2)))
     return (
         <WingBlank className="goods-list-two">
-            <div>
+            {/* <div>
                 {
                     proListleft.map(el => (
                         <img src={el.imgUrl} key={el.goodsId}  alt=""/>
@@ -35,7 +35,7 @@ export default function (props: Props) {
                         <img src={el.imgUrl} key={el.goodsId}  alt=""/>
                     ))
                 }
-            </div>
+            </div> */}
         </WingBlank>
     )
 }
